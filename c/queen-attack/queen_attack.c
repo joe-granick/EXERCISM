c3d4d06069d1cc6c9c1e46beecfff428498729b1
joe-granick <jgranick@pop-os.mynetworksettings.com>
2025-01-22 17:41:24 -0500

    queen-attack solved

--- c/queen-attack/queen_attack.c
+++ c/queen-attack/queen_attack.c
@@ -1,14 +1,24 @@
-#include "queen_attack.h
+#include "queen_attack.h"
 
 attack_status_t can_attack(position_t queen_1, position_t queen_2) {
 
-  if (queen_1->row == queen_2-> && queen_1->column == queen_2->column)
+  if (queen_1.row == queen_2.row && queen_1.column == queen_2.column)
   { return INVALID_POSITION;}
 
-  if (queen_1->row > 7 || queen_1->column > 7 || queen_2-row > 7 || queen_2->column > 7)
+  if (queen_1.row > 7 || queen_1.column > 7 || queen_2.row > 7 || queen_2.column > 7)
   {return INVALID_POSITION;}
 
-  if (queen_1->row < 0 || queen_1->column < 0 || queen_2-row < 0 || queen_2->column  < 0)
+  if (queen_1.row < 1 || queen_1.column < 1 || queen_2.row < 1 || queen_2.column  < 1)
   {return INVALID_POSITION;}
+  
+  if (queen_1.row == queen_2.row)
+  {return CAN_ATTCK;}
+  
+  if (queen_1.column == queen_2.column)
+  {return CAN_ATTACK;}
 
+  if ((queen_1.column-queen_2.column)/(queen_1.row -queen_2.row))
+  {return CAN_ATTCK;}
+
+return CAN_NOT_ATTACK;:
 }
