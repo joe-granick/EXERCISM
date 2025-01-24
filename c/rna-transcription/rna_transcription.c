9f5ec14c0be2a11acc5cdbc8bcbcd6e0f437fc85
joe-granick <jmgranick@gmail.com>
2025-01-24 15:39:57 -0500

    initial implemenation RNA transcription, untested

--- c/rna-transcription/rna_transcription.c
+++ c/rna-transcription/rna_transcription.c
@@ -1,1 +1,32 @@
 #include "rna_transcription.h"
+
+
+char *to_rna(const char* dna){
+  int i = 0;
+  char* rna;
+  while(*(dna+i)) {
+    switch (*(dna+i)) {
+      case 'G':
+        *(rna+i) = 'C';
+        break;
+
+      case 'G':
+        *(rna+i) = 'C';
+        break;
+      
+      case 'C':
+        *(rna+i) = 'G';
+        break;
+
+      case 'T':
+        *(rna+i) = 'A';
+        break;
+      default :
+        break;
+    }
+
+    ++i;
+  }
+  *(rna+i) = '/0';
+  return rna;
+}
