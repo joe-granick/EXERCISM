7765a684a792aa90661343df4f7b54e2a288e294
joe-granick <jmgranick@gmail.com>
2025-01-25 15:53:56 -0500

    13 - Eliud's eggs solved

--- c/eliuds-eggs/eliuds_eggs.c
+++ c/eliuds-eggs/eliuds_eggs.c
@@ -1,1 +1,12 @@
 #include "eliuds_eggs.h"
+    unsigned int egg_count(unsigned int i)
+    {
+        unsigned int count = 0;
+        while (i>0)
+        {
+            if (i%2) {count++;}
+            i/=2;
+        }
+        
+        return count;
+    }
