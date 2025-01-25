f7fe30474cabb052d18ef5dd48f35bc2a91d206a
joe-granick <jmgranick@gmail.com>
2025-01-25 15:36:12 -0500

    12 - Binary Solved

--- c/binary/binary.c
+++ c/binary/binary.c
@@ -1,1 +1,17 @@
 #include "binary.h"
+
+int convert(const char *input){
+    int base = 1;
+    int sum = 0;
+    int l = 0;
+    while(*(input+l))
+    {l++;}
+    l = l-1;
+    while(l>=0)
+    {
+        if (*(input+l) > '1') return -1;
+        if(*(input+l) == '1'){sum += base;}
+         base*=2;
+         l--;}
+    return sum;
+}
