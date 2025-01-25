a3d1dd02d541c6dcd3ea164fcfc2ecc508366cd5
joe-granick <jmgranick@gmail.com>
2025-01-25 17:50:04 -0500

    14 - Two Fer better solution with ternary operator instead of conditional

--- c/two-fer/two_fer.c
+++ c/two-fer/two_fer.c
@@ -3,8 +3,5 @@
 #include <stdio.h>
 
 void two_fer(char *buffer, const char *name) {
-    if (name == NULL || *name == '\0')
-    {sprintf(buffer,  "One for you, one for me.");}
-    else 
-    {sprintf(buffer,  "One for %s, one for me.", name);}
+    sprintf(buffer,  "One for %s, one for me.", name ? name : "you");
   }
