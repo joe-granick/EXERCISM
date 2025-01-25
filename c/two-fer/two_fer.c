c457c8245c2d30fcb60ca65ed317073f5b183b3e
joe-granick <jmgranick@gmail.com>
2025-01-25 17:44:39 -0500

    14 - Two Fer solved with sprintf

--- c/two-fer/two_fer.c
+++ c/two-fer/two_fer.c
@@ -1,1 +1,10 @@
 #include "two_fer.h"
+#include <string.h>
+#include <stdio.h>
+
+void two_fer(char *buffer, const char *name) {
+    if (name == NULL || *name == '\0')
+    {sprintf(buffer,  "One for you, one for me.");}
+    else 
+    {sprintf(buffer,  "One for %s, one for me.", name);}
+  }
--- c/two-fer/two_fer.h
+++ c/two-fer/two_fer.h
@@ -1,6 +1,6 @@
 #ifndef TWO_FER_H
 #define TWO_FER_H
-
+#define BUFFER_SIZE (100)
 void two_fer(char *buffer, const char *name);
 
 #endif
