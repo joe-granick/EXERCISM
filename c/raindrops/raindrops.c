fdd8dd3f08f13244c7cab200a14f4563b1a7eddd
joe-granick <jmgranick@gmail.com>
2025-01-25 21:47:46 -0500

    15 -Raindrops initial test passed

--- c/raindrops/raindrops.c
+++ c/raindrops/raindrops.c
@@ -1,1 +1,22 @@
 #include "raindrops.h"
+#include <stdio.h>
+#include <string.h>
+
+
+void convert(char result[], int drops) {
+  if (!(drops%3)) {
+      sprintf(result, "%s", "Pling");
+  }
+
+  if(!(drops%5)) {
+    sprintf(result, "%s", "Plang");
+  }
+
+  if(!(drops%7)) {
+    sprintf(result, "%s", "Plong");
+  }
+
+  if((drops%3) && (drops%5) && (drops%7)) {
+    sprintf(result, "%d", drops);
+  }
+}
