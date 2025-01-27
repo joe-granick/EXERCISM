73441321e800b931b1a32a68d3184ee7251b6309
joe-granick <jmgranick@gmail.com>
2025-01-27 14:44:29 -0500

    20 - Pangram first test passed

--- c/pangram/pangram.c
+++ c/pangram/pangram.c
@@ -1,1 +1,31 @@
 #include "pangram.h"
+#include <string.h>
+#include <ctype.h>
+bool is_pangram(const char* sentence)
+{
+  if (sentence == NULL)
+  {
+    return 0;
+  }
+  char alphabet[26] = {0};
+  int i = 0;
+  int alpha_index = 0;
+  while(sentence[i] != '\0')
+  {
+    if (isalpha(sentence[i]))
+    {
+      alpha_index = tolower(sentence[i]) - 'a';
+      alphabet[alpha_index] = 1;
+    }
+    i++;
+  }
+
+  for (int j = 0; j < 26; j++)
+  {
+    if (alphabet[j]==0)
+    {return 0;}
+  }
+ 
+  return 1;
+
+}
