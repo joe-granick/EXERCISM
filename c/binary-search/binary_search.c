8e7e0f7448182ca3854719fe6ff31a49db384887
joe-granick <jmgranick@gmail.com>
2025-01-28 14:59:07 -0500

    24 Binary Search solved

--- c/binary-search/binary_search.c
+++ c/binary-search/binary_search.c
@@ -1,1 +1,33 @@
 #include "binary_search.h"
+const int *binary_search(int value, const int *arr, size_t length)
+{
+    if (length == 0) // Handle empty array
+        return NULL;
+    size_t l = 0;
+    size_t r = length-1;
+    int val_ind = search(value, arr, l, r);
+    if (val_ind == -1)
+    {return NULL;}
+    return &arr[val_ind];
+}
+
+int search(int value, const int *arr, size_t l, size_t r)
+{   
+    if(l > r) {
+        return -1;
+    }
+    int m = l+(r-l)/2;
+    if (arr[m] == value)
+    {return m;}
+
+    if (arr[m]<value)
+    {
+        return search(value, arr,m+1, r);
+    }
+
+    if (arr[m]> value)
+    {
+        return search(value,arr,l,m-1);
+    }
+    return -1;
+}
--- c/binary-search/binary_search.h
+++ c/binary-search/binary_search.h
@@ -4,5 +4,5 @@
 #include <stddef.h>
 
 const int *binary_search(int value, const int *arr, size_t length);
-
+int search(int value ,const int *arr, size_t l, size_t r);
 #endif
