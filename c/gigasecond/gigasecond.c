040c998b3cfa49a1312b667fd1128bcc72d6af62
joe-granick <jmgranick@gmail.com>
2025-01-29 17:55:29 -0500

    26 gigasecond solved with localtime (investigate gm time altyernative solution)

--- c/gigasecond/gigasecond.c
+++ c/gigasecond/gigasecond.c
@@ -1,1 +1,8 @@
 #include "gigasecond.h"
+#include <math.h>
+void gigasecond(time_t input, char *output, size_t size){
+    time_t seconds = input + pow(10,9);
+    struct tm *time;
+    time = localtime(&seconds);
+    strftime(output,size,"%Y-%m-%d %H:%M:%S", time);
+}
