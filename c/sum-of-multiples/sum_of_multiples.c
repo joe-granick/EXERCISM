f2275b30dff71b6b3a907aa247521fb4951a9e52
joe-granick <jmgranick@gmail.com>
2025-01-31 12:41:53 -0500

    31 sum of multiples, first test passed, may still need to implements duplicate check (can probably be done w/ bool array or sorting

--- c/sum-of-multiples/sum_of_multiples.c
+++ c/sum-of-multiples/sum_of_multiples.c
@@ -1,1 +1,17 @@
 #include "sum_of_multiples.h"
+
+
+unsigned int sum(const unsigned int *factors, const size_t number_of_factors,const unsigned int limit)
+{
+  unsigned int m =1;
+  unsigned int sum = 0;
+  size_t i = 0;
+  while(i < number_of_factors)
+  {
+    unsigned int fac = factors[i];
+    while((m*fac) < limit)
+    {sum+=(m*fac);m++;}
+    i++;
+  }
+  return sum;
+}
