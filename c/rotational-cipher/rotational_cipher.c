89b79ccd42da851915cabd62f343bc783afb898e
joe-granick <jmgranick@gmail.com>
2025-02-04 11:53:40 -0500

    33 Rotational Cypher all tests passed

--- c/rotational-cipher/rotational_cipher.c
+++ c/rotational-cipher/rotational_cipher.c
@@ -1,13 +1,26 @@
 #include "rotational_cipher.h"
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 
 char *rotate(const char *text, int shift_key){
-  char* cypher = malloc(sizeof(char)*strlen(text));
-  for(int i = 0; text[i]!='\0'; i++){
-      cypher[i] = ((text[i] -'a')+shift_key)%26 + 'a';
+  size_t len = strlen(text);
+  char* cypher = malloc(sizeof(char)*(len+1));
+  
+  if(!cypher){ return NULL;}
+
+  for(size_t i = 0; i <  len; i++){
+      if(isalpha(text[i]))
+      {
+      if(islower(text[i]))
+        cypher[i] = ((text[i] - 'a')+shift_key)%26 + 'a';
+      else 
+        cypher[i] = ((text[i] - 'A')+shift_key)%26 + 'A';
+      }
+      else{cypher[i] = text[i];}
   }
+  cypher[len] = '\0';
   
   return cypher;
 }
