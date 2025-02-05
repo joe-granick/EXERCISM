33947b775ef5e76f84ba572c22b877e82e81106b
joe-granick <jmgranick@gmail.com>
2025-02-05 05:37:39 -0500

    35 square root intital test case passed

--- c/square-root/square_root.c
+++ c/square-root/square_root.c
@@ -1,1 +1,32 @@
 #include "square_root.h"
+/* square root calculation interface
+ * takes an integer returns the square root
+ * calls other function to calculate root for simple refactoring
+ * currently uses newton raphson method, but can
+ * potentially be changed to iterative integer, secant, bisection, or other 
+ * root finding method once implemented*/
+
+int square_root(int n){
+  
+  int root = newton_raphson(n);
+  return root;
+}
+
+/*newton raphson finds root of number
+ * by incrementally taking dividing by the
+ * function derivative until value converges
+ * on the root*/
+int newton_raphson(int n){
+    double x0;
+    double x = (double)n;
+    double fx = x*x;
+    double dx = 2*x;
+    while(x -(fx/dx)){
+    x0 = x;
+    x = x-fx/dx;}
+
+    return (int)x0;
+
+
+    
+}
--- c/square-root/square_root.h
+++ c/square-root/square_root.h
@@ -1,4 +1,5 @@
 #ifndef SQUARE_ROOT_H
 #define SQUARE_ROOT_H
-
+int square_root(int n);
+int newton_raphson(int n);
 #endif
