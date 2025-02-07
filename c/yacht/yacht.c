5a4f457f4a81c5f044207686a55a8562b9a64da1
joe-granick <jmgranick@gmail.com>
2025-02-07 12:33:24 -0500

    43 Yacht Dice all tests passed

--- c/yacht/yacht.c
+++ c/yacht/yacht.c
@@ -5,65 +5,68 @@ int score(dice_t dice, category_t category){
   int rolls[6];
   for(int i = 0; i < 6; i++)
       {rolls[i] = 0;}
-  for(int i = 0; i < 6; i++)
-  {
-    rolls[dice.faces[i]-1] += 1;
-  }
-    category_t face;
+
+  for(int i = 0; i < 5; i++)
+  {rolls[dice.faces[i]-1] += 1;}
+    int face = 1;
   switch(category){
     case ONES:
-      face = ONES;
+      face = 1;
       score+=rolls[face-1]*face;
       break;
 
     case TWOS:
-      face = TWOS;
+      face = 2;
       score+=rolls[face-1]*face;
       break;
     
     case THREES:
-      face = THREES;
+      face = 3;
       score+=rolls[face-1]*face;
       break;
     
     case FOURS:
-      face = FOURS;
+      face = 4;
       score+=rolls[face-1]*face;
       break;
 
     case FIVES:
-      face = FIVES;
+      face = 5;
       score+=rolls[face-1]*face;
       break;
 
     case SIXES:
-      face = SIXES;
+      face = 6;
       score+=rolls[face-1]*face;
       break;
     
     case FULL_HOUSE:
       bool three = false;
       bool two = false;
+      int three_val;
+      int two_val;
       for(int i = 0; i <6; i++)
-      {if(rolls[i]==3) three = true;
-      if(rolls[i]==2) two = true;}
-      if (two && three) score+=19;
+      {if(rolls[i]==3){three = true; three_val=i;}
+      if(rolls[i]==2){two = true; two_val=i;}}
+      if (two && three){ 
+        if (three_val > two_val) score+=16;
+        else score+=19;}
       break;
     
     case FOUR_OF_A_KIND:
       for(int i = 0; i <6; i++)
-      {if(rolls[i]==4){score+=16; break;}}
+      {if(rolls[i]>=4){score+=4*(i+1); return score;}}
       break;
     
     case LITTLE_STRAIGHT:
       for(int i = 0; i <5; i++)
-      {if(rolls[i]!=1) break;}
+      {if(rolls[i]!=1) return 0;}
       score+=30;
       break;
     
     case BIG_STRAIGHT:
       for(int i = 1; i <6; i++)
-      {if(rolls[i]!=1) break;}
+      {if(rolls[i]!=1) return 0;}
       score+=30;
       break;
     
@@ -73,11 +76,13 @@ int score(dice_t dice, category_t category){
       break;
 
     case YACHT:
-      for(int i = 0; i < 6; i++)
-        if(rolls[i] == 5){score+=50; break;}
+      for(int i = 0; i < 6; i++){
+        if(rolls[i] == 5){score+=50; return score;}
+      }
+      return 0;
       break;
     
-        default:
+    default:
       score = 0;
       break;
   }
