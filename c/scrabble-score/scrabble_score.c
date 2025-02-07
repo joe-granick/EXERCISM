319686fc75f37315df4809413c10fb65c6aa6acb
joe-granick <jmgranick@gmail.com>
2025-02-07 12:43:57 -0500

    44 scrabble score initial tets passed

--- c/scrabble-score/scrabble_score.c
+++ c/scrabble-score/scrabble_score.c
@@ -1,1 +1,17 @@
 #include "scrabble_score.h"
+
+unsigned int score(const char *word){
+int i = 0;
+int score = 0;
+while(word[i]!='\0')
+{
+  char let = word[i];
+  if(let == 'Q' || let == 'Z') score+= 10;
+  else if(let == 'J' || let == 'X') score+=8;
+  else if(let == 'K') score+=5;
+  else if(let == 'F' || let == 'H' || let == 'V' || let == 'W' || let == 'Y') score += 4;
+  else if(let == 'B' || let == 'C' || let == 'M' || let == 'P')score +=3;
+  else if(let == 'D' || let == 'G') score +=2;
+  else score +=1;
+  i++;
+}return score;}
